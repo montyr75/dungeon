a9c726e7b3df125b483a9f13605d4359f966cc7b
Monty Rasmussen <montyr75@gmail.com>
2014-10-24 15:38:52 -0600

    attack working

--- lib/views/card_view/card_view.dart
+++ lib/views/card_view/card_view.dart
@@ -1,7 +1,9 @@
 library card_view;
 
 import 'package:polymer/polymer.dart';
+import 'package:core_elements/core_dropdown.dart';
 import 'dart:html';
+import 'dart:math' as Math;
 import '../../model/cards.dart';
 
 @CustomTag('card-view')
@@ -15,6 +17,7 @@ class CardView extends PolymerElement {
   @published Card card;
 
   @observable String cardImagePath;
+  @observable String attackText;
 
   CardView.created() : super.created();
 
@@ -27,6 +30,7 @@ class CardView extends PolymerElement {
     print("$CLASS_NAME::cardChanged()");
 
     if (card == null) {
+      cardImagePath = null;
       return;
     }
 
@@ -44,6 +48,39 @@ class CardView extends PolymerElement {
 
   void discard(Event event, var detail, Element target) {
     fire('discard', detail: card);
+  }
+
+  void attack(Event event, var detail, Element target) {
+    print("$CLASS_NAME::attack()");
+
+    CoreDropdown popup = shadowRoot.querySelector("#attack-popup");
+    popup.opened = !popup.opened;
+
+    if (!popup.opened) {
+      return;
+    }
+
+    int attackRoll = _rollDie(6) + _rollDie(6);
+
+    switch (attackRoll) {
+      case 0:
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+      case 5: attackText = "<strong>Miss!</strong> No effect."; break;
+      case 6:
+      case 7: attackText = "<strong>Stunned:</strong> Drop 1 random Treasure card."; break;
+      case 8:
+      case 9:
+      case 10: attackText = "<strong>Wounded:</strong> Drop 1 random Treasure card and move your Hero back 1 space in the direction he or she came. Take 1 Lose a Turn token."; break;
+      case 11: attackText = "<strong>Seriously Wounded:</strong> Randomly drop half your Treasure cards (round up) and place your Hero on the Great Hall."; break;
+      case 12: attackText = "<strong>Killed!</strong> Drop all your Treasure cards. Choose a new Hero (or the same one if no other Hero is available) and place it on the Great Hall."; break;
+    }
+  }
+
+  int _rollDie(int sides) {
+    return new Math.Random().nextInt(sides) + 1;
   }
 
   String get monster => Card.MONSTER;
